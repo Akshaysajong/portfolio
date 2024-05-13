7f5a64e798d3cc481b9b4929eb681325dd7ade47
Akshaysajong <akshaysajong@example.com>
2024-05-13 13:15:45 +0530

    3rd coomit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-*bs(l!owxj^6^30$6_o-($&x=^_hs4-29_^mjb_78q2(_d&65j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.26.205.157', '3.26.205.157:8000']
 
 
 # Application definition
