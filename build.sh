8b51d5823bbc1e05a3f36c1c2257d91477f9c82f
Akshaysajong <akshaysajong@gmail.com>
2026-04-24 15:54:14 +0530

    fix superuser creation

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
 python manage.py migrate
 
-python manage.py createsuperuser \
-    --noinput \
-    --username admin \
-    --email admin@example.com || true
+# FORCE create/update admin user
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+u, created = User.objects.get_or_create(username='admin');
+u.set_password('admin123');
+u.is_staff = True;
+u.is_superuser = True;
+u.save()" | python manage.py shell
